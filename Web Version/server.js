93f6ecef29f5934f178110f60aed6d5b7d56c6c3
peelstnac <chfrn2002@gmail.com>
2020-06-08 23:03:10 -0400

    added MongoDB model

--- Web Version/server.js
+++ Web Version/server.js
@@ -1,8 +1,33 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-const Leaderboard = require('./MongoDB/leaderboardSchema.js');
-const PORT = 3000 //process.env.PORT;
+const Leaderboard = require('./leaderboardSchema.js');
+
+const URI = process.env.URI
+const PORT = process.env.PORT;
+
+function init() {
+    var leaderboard = new Leaderboard({
+        name: 'leaderboard',
+        val: []
+    });
+    leaderboard.save((err) => {
+        console.log('saved leaderboard');
+        if(err) {
+            console.log(err);
+        }
+    })
+}
+
+mongoose.connect(URI, {useNewUrlParser: true, useUnifiedTopology: true});
+var db = mongoose.connection;
+db.on('error', (err) => {
+    console.log(err);
+});
+db.on('open', () => {
+    console.log('connected to MongoDB');
+    //init();
+});
 
 app.listen(PORT, (err) => {
     console.log('listening to port ' + PORT);
